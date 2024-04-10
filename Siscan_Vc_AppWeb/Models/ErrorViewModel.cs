9a8b9007194ea91bc916e85974128cd6fd3e4345
jeiderws <134397655+jeiderws@users.noreply.github.com>
2024-04-10 13:36:53 -0500

    Agregar archivos de proyecto.

---
+++ Siscan_Vc_AppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Siscan_Vc_AppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
