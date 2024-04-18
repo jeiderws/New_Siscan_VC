eb6e1fc0068cb12de650a3f29ae6dd157f44ebd1
guidostark37 <bguido10@gmail.com>
2024-04-18 08:44:20 -0500

    creacion de controlador aprendiz

---
+++ Siscan_Vc_AppWeb/Controllers/AprendizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Siscan_Vc_AppWeb.Controllers
+{
+    public class AprendizController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
