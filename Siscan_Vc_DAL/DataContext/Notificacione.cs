a69fef7c447569f83b4f5c883c335987c3b86c81
Dillon Duncan <dillonduncan0607@gmail.com>
2024-04-18 08:47:21 -0500

    BD Actualizada

---
+++ Siscan_Vc_DAL/DataContext/Notificacione.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Siscan_Vc_DAL.DataContext;
+
+public partial class Notificacione
+{
+    public int IdNotificacion { get; set; }
+
+    public string? DescripcionNotificion { get; set; }
+
+    public int IdEstado { get; set; }
+
+    public virtual EstadoAprendiz IdEstadoNavigation { get; set; } = null!;
+}
---
+++ Siscan_Vc_DAL/DataContext/SeguimientoInstructorAprendiz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Siscan_Vc_DAL.DataContext;
+
+public partial class SeguimientoInstructorAprendiz
+{
+    public long? IdSeguimiento { get; set; }
+
+    public string NumeroDocumentoAprendiz { get; set; } = null!;
+
+    public string NumeroDocumentoInstructor { get; set; } = null!;
+
+    public int? IdCoformador { get; set; }
+
+    public DateOnly? FechaInicio { get; set; }
+
+    public DateOnly? FechaFinalizacion { get; set; }
+
+    public int? IdModalidad { get; set; }
+
+    public long? IdAsignacionArea { get; set; }
+
+    public int? IdAreaEmpresa { get; set; }
+
+    public string? NitEmpresa { get; set; }
+
+    public virtual AreasEmpresa? IdAreaEmpresaNavigation { get; set; }
+
+    public virtual AsignacionArea? IdAsignacionAreaNavigation { get; set; }
+
+    public virtual Coformador? IdCoformadorNavigation { get; set; }
+
+    public virtual Modalidad? IdModalidadNavigation { get; set; }
+
+    public virtual Empresa? NitEmpresaNavigation { get; set; }
+
+    public virtual Aprendiz NumeroDocumentoAprendizNavigation { get; set; } = null!;
+
+    public virtual Instructor NumeroDocumentoInstructorNavigation { get; set; } = null!;
+}
