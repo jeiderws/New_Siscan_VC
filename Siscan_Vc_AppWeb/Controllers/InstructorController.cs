953233117d29527c76d753bf3caa4006c2d83d12
jeiderws <134397655+jeiderws@users.noreply.github.com>
2024-04-26 14:39:07 -0500

    Vistas

---
+++ Siscan_Vc_AppWeb/Controllers/InstructorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Siscan_Vc_AppWeb.Controllers
+{
+    public class InstructorController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
