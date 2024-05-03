92c94afbf3f05baeb81bac4dcb5a32b23a43c85f
Dillon Duncan <dillonduncan0607@gmail.com>
2024-05-03 12:23:34 -0500

    ViewModelEmpresa

---
+++ Siscan_Vc_AppWeb/Models/ViewModels/ViewModelEmpresa.cs
@@ -0,0 +1,38 @@
+﻿using Siscan_Vc_DAL.DataContext;
+
+namespace Siscan_Vc_AppWeb.Models.ViewModels
+{
+    public class ViewModelEmpresa
+    {
+        public ViewModelEmpresa(Empresa empresa)
+        {
+            Nitmpresa=empresa.Nitmpresa;
+
+            NombreEmpresa=empresa.NombreEmpresa;
+
+            RepresentanteLegal=empresa.RepresentanteLegal;
+
+            DireccionEmpresa = empresa.DireccionEmpresa;
+
+            TelefonoEmpresa = empresa.TelefonoEmpresa;
+
+            IdCiudad=empresa.IdCiudad;
+
+            NombreEmpresa = empresa.IdCiudadNavigation.NombreCiudad;
+            
+        }
+        public string Nitmpresa { get; set; } = null!;
+
+        public string? NombreEmpresa { get; set; }
+
+        public string? RepresentanteLegal { get; set; }
+
+        public string? DireccionEmpresa { get; set; }
+
+        public string? TelefonoEmpresa { get; set; }
+
+        public int? IdCiudad { get; set; }
+
+        public string? NombreCiudad { get; set; }
+    }
+}
