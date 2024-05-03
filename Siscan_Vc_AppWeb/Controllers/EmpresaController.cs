d00e18e99cfcfd8692efcc6d6f982c6845874647
guidostark37 <bguido10@gmail.com>
2024-05-03 12:26:43 -0500

    nuevo cambio vista empresa

--- Siscan_Vc_AppWeb/Controllers/EmpresaController.cs
+++ Siscan_Vc_AppWeb/Controllers/EmpresaController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Siscan_Vc_BLL.Service.InterfacesService;
+using Siscan_Vc_DAL.DataContext;
 
 namespace Siscan_Vc_AppWeb.Controllers
 {
     public class EmpresaController : Controller
     {
+
+        
+        private readonly DbSiscanContext _dbSiscanContext;
+        public EmpresaController(DbSiscanContext dbSiscanContext)
+        {
+            _dbSiscanContext = dbSiscanContext;
+        }
+
         public IActionResult Registro()
         {
             return View();
@@ -12,6 +23,17 @@ namespace Siscan_Vc_AppWeb.Controllers
         public IActionResult consultar()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ObtenerNombresEmpresa(string term)
+        {
+            var nombreEmpresa = _dbSiscanContext.Empresas
+                .Where(e => e.NombreEmpresa.Contains(term))
+                .Select(e => e.NombreEmpresa)
+                .ToList();
+
+            return Json(nombreEmpresa);
         }
     }
 }
---
+++ Siscan_Vc_AppWeb/Controllers/SeguimientoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Siscan_Vc_AppWeb.Controllers
+{
+    public class SeguimientoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
