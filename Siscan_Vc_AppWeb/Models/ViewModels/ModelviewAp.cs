5af1a6b2bdc752d1585b760060ba566fd7010210
jeiderws <134397655+jeiderws@users.noreply.github.com>
2024-05-08 08:20:12 -0500

    Tabla de Consulta

---
+++ Siscan_Vc_AppWeb/Models/ViewModels/ModelviewAp.cs
@@ -0,0 +1,10 @@
+﻿using Siscan_Vc_DAL.DataContext;
+
+namespace Siscan_Vc_AppWeb.Models.ViewModels
+{
+    public class ModelviewAp
+    {
+        public List<ViewModelAprendiz> ListaAprendices { get; set; }
+        public Aprendiz Aprendiz { get; set; }
+    }
+}
