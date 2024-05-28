2805a55af46c7e0da5403d10d286a0cd10714ade
guidostark37 <bguido10@gmail.com>
2024-05-28 10:14:10 -0500

    taka taka

---
+++ Siscan_Vc_AppWeb/Controllers/ProgramasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Siscan_Vc_AppWeb.Controllers
+{
+    public class ProgramasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
