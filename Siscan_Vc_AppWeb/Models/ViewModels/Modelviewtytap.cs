acf24e1b58a9768a69d25b2741f5ad16e5488a43
Dillon Duncan <dillonduncan0607@gmail.com>
2024-06-04 08:28:25 -0500

    view model tytap

--- Siscan_Vc_AppWeb/Models/ViewModels/Modelviewtytap.cs
+++ Siscan_Vc_AppWeb/Models/ViewModels/Modelviewtytap.cs
@@ -5,21 +5,26 @@ namespace Siscan_Vc_AppWeb.Models.ViewModels
 {
     public class Modelviewtytap
     {
+
+        //opciones seleccionadas en los combos
         public int OpcSeleccionadoTpDoc { get; set; }
-        public List<SelectListItem> listaOpcTpDoc { get; set; }
-        public int OpcSeleccionadoEstado { get; set; }
-        public List<SelectListItem> listaOpcEstado { get; set; }
         public int OpcSeleccionadoCiudad { get; set; }
         public int OpcSeleccionadoCiudadTyt { get; set; }
-        public List<SelectListItem> listaOpcCiudad { get; set; }
+        public int OpcSeleccionadoEstado { get; set; }
         public int OpcSeleccionadoEstadoTyt { get; set; }
-        public List<SelectListItem> listaOpcEstadoTyt { get; set; }
         public int OpcSeleccionadoFicha { get; set; }
-        public List<SelectListItem> listaOpcFicha { get; set; }
         public int OpcSeleccionadoConvocatoria { get; set; }
+
+        //listas selecionables de los combos
+        public List<SelectListItem> listaOpcTpDoc { get; set; }
+        public List<SelectListItem> listaOpcEstado { get; set; }
+        public List<SelectListItem> listaOpcCiudad { get; set; }
+        public List<SelectListItem> listaOpcEstadoTyt { get; set; }
+        public List<SelectListItem> listaOpcFicha { get; set; }
         public List<SelectListItem> listaOpcConvocatoria { get; set; }
         public List<SelectListItem> listaOpcPrograma { get; set; }
         public List<SelectListItem> listaOpcDepartamento { get; set; }
+
         public Aprendiz aprendiz { get; set; }
         public InscripcionTyt? inscripcionTyt { get; set; }
     }
