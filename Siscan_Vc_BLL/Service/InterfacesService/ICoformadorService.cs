85885f9521320bf79eee94eb3bd34ba2f599ec24
Dillon Duncan <dillonduncan0607@gmail.com>
2024-07-11 13:37:21 -0500

    servicio coformador

---
+++ Siscan_Vc_BLL/Service/InterfacesService/ICoformadorService.cs
@@ -0,0 +1,20 @@
+﻿using Siscan_Vc_DAL.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siscan_Vc_BLL.Service.InterfacesService
+{
+    public interface ICoformadorService
+    {
+        Task<bool> Insert(Coformador model);
+        Task<bool> Update(Coformador model);
+        Task<bool> Delete(string numDoc);
+        Task<Coformador> GetForDoc(string numeroDoc);
+        Task<IQueryable<Coformador>> GetAll();
+        Task<Coformador> GetForName(string name);
+        Task<Coformador> GetForEmpresa(string nitEmpresa);
+    }
+}
